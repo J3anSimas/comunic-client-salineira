2684255b3627bfdb3493a6c0244dc66bf8879168
Jean Simas <jeansimas16@gmail.com>
2022-06-12 16:01:33 -0300

    First Commit

---
+++ ComunicClientSalineira/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ComunicClientSalineira
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "Close";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ ComunicClientSalineira/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace ComunicClientSalineira
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        long lastChange;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void WriteLastModfReg()
+        {
+            StreamWriter streamWriter = new StreamWriter("lastModifiedRegistry.bin");
+            DateTime fileChangeDate = File.GetLastWriteTime(path);
+            streamWriter.Write(ConvertDateToUnixTS(fileChangeDate).ToString());
+            streamWriter.Close();
+            ReadLastModReg();
+        }
+        
+        private long ConvertDateToUnixTS(DateTime date)
+        {
+            return ((DateTimeOffset)date).ToUnixTimeSeconds();
+        }
+        private void ReadLastModReg()
+        {
+            StreamReader streamReader = new StreamReader("lastModifiedRegistry.bin");
+            string lastMod = streamReader.ReadToEnd();
+            streamReader.Close();
+            lastChange = (long)Convert.ToDouble(lastMod);
+        }
+
+        private void PrintMessageContent()
+        {
+            StreamReader streamReader = new StreamReader(path);
+            string message = streamReader.ReadToEnd();
+            streamReader.Close();
+            MessageBox.Show(message);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader streamReader = new StreamReader("messagePath.bin");
+            string messagePath = streamReader.ReadToEnd();
+            streamReader.Close();
+            path = Path.GetFullPath(messagePath);
+
+            if (File.Exists("lastModifiedRegistry.bin"))
+            {
+                ReadLastModReg();
+            }
+            else
+            {
+                WriteLastModfReg();
+            }
+            timer1.Enabled = true;
+
+        }
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            DateTime fileChangeDate = File.GetLastWriteTime(path);
+            label1.Text = ConvertDateToUnixTS(fileChangeDate).ToString() + " | " + lastChange;
+            if (ConvertDateToUnixTS(fileChangeDate) - lastChange != 0)
+            {
+               WriteLastModfReg();
+               PrintMessageContent();
+
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
